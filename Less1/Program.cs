8c0a404dc79d3fcd94780307fee3f77f5af034cb
Viktor <deminvutya@gmail.com>
2022-06-24 15:45:52 +0700

    Занятие

--- Less1/Program.cs
+++ Less1/Program.cs
@@ -1,3 +1,10 @@
 ﻿
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+// Console.Write("ВВедите число: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+// int num = int.Parse(Console.ReadLine());
+// int squer = num * num;
+// Console.WriteLine($"Квадрат числа {num} = {squer}");
+
 
