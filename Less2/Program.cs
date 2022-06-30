bdaed04091686b646f714c4edb0e8cb853d94b5f
Viktor <deminvutya@gmail.com>
2022-06-30 15:11:20 +0700

    Занятие 2

--- Less2/Program.cs
+++ Less2/Program.cs
@@ -1,3 +1,27 @@
 ﻿
 int rndNum = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка 10 - 99 => {rndNum}");
 
+// int firstDigit = rndNum / 10;
+// int secontDigit = rndNum % 10;
+// int maxDigit = firstDigit > secontDigit ? firstDigit : secontDigit;
+// if(firstDigit > secontDigit)
+// {
+//    maxDigit = firstDigit;
+// }
+// else
+// {
+//    maxDigit = secontDigit;
+// }
+
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secontDigit = num % 10;
+    if (firstDigit == secontDigit) return 0;
+    return firstDigit > secontDigit ? firstDigit : secontDigit;
+}
+int maxDigit = MaxDigit(rndNum);
+String result = maxDigit > 0 ? maxDigit.ToString() : "Цыфры равны";
+Console.WriteLine($"Наибольшая цыфра числа {rndNum} => {result}");
