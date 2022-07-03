cdc55fc36307671de51d6442737203d38cd80633
Viktor <deminvutya@gmail.com>
2022-07-03 20:23:19 +0700

    Домашнее задание 3

---
+++ HomeWork/HomeWork3/Task23/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход 
+// число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] arry = new int[num + 1];
+Cubic(arry);
+PrintArry(arry);
+
+void Cubic(int[] num)
+{
+    int counter = 0;
+    int length = num.Length;
+    while (counter < length)
+    {
+        num[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+        counter++;
+    }
+}
+void PrintArry(int[] arr)
+{
+    int count = arr.Length;
+    int index = 0;
+    while (index < count)
+    {
+        Console.Write(arr[index] + " ");
+        index++;
+    }
+}
