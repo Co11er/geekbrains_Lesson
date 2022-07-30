eeacf708387075a9611b4050cf2c7bdfb9f39350
Viktor <deminvutya@gmail.com>
2022-07-30 11:34:20 +0700

    Less9

---
+++ Lesson/Less9/Task63/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Clear();
+Console.Write("Задайте число");
+int num =int.Parse(Console.ReadLine());
+
+
+void NaturalNumber(int num)
+{
+   if(num == 0) return;
+   NaturalNumber(num - 1);
+   Console.Write($"{num} ");
+}
